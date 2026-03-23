5ee4d16676f58ebb34a6a6199c55008c2daff6a5
Manikrao Habal <manikraohabala@gmail.com>
2026-03-23 22:12:03 +0530

    Increment Program

---
+++ src/assignments/Increment.java
@@ -0,0 +1,15 @@
+package assignments;
+
+public class Increment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		System.out.println("Value of \"a\":"+a);
+		a=a+2;
+		System.out.println("After increment: \t "+a);
+		
+		
+	}
+
+}
