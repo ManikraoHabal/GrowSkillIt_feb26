2d3dc39263852fa0778d1d691977733bdd79ecef
Manikrao Habal <manikraohabala@gmail.com>
2026-03-25 22:15:23 +0530

    Looping program to print the square, cube and num-5 for respective condition

---
+++ src/assignments/LooptoPrintNumbers.java
@@ -0,0 +1,34 @@
+package assignments;
+
+public class LooptoPrintNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=1;
+		
+		while(num<=50)
+		{
+			if(num%2==0 && num%5==0)
+			{
+				System.out.println(num*num);
+			}
+			else if(num%2==0)
+			{
+				System.out.println(num*num*num);
+			}
+			else if(num%5==0)
+			{
+				System.out.println(num-5);
+			}
+			else
+			{
+				System.out.println(num);
+			}
+			num++;
+		}
+		
+		
+		
+	}
+
+}
